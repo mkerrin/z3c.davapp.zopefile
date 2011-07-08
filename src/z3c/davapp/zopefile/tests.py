d1c68e6ffb96d770ca7f966c385effbd3c951e97
Michael Kerrin <michael.kerrin@gmail.com>
2011-07-08 15:11:15 +0100

    since I use the latest version of ZODB, it supports blobs directly so that I don't have to use zope.file.testing to setup a blob ZODBLayer. This is handy as I don't have to depend on zope.app.testing now.

--- src/z3c/davapp/zopefile/tests.py
+++ src/z3c/davapp/zopefile/tests.py
@@ -1,33 +1,38 @@
-import os
 import unittest
 import doctest
 
-import zope.file.testing
-
 import z3c.etree.testing
 import z3c.dav.testing
+import z3c.davapp.zopefile
 
-here = os.path.dirname(os.path.realpath(__file__))
-ZopeFileDAVLayer = z3c.dav.testing.WebDAVLayerClass(
-    os.path.join(here, "ftesting.zcml"), __name__, "ZopeFileDAVLayer")
+davlayer = z3c.dav.testing.WebDAVLayer(z3c.davapp.zopefile)
+
+
+def setUp(test):
+    z3c.dav.testing.functionalSetUp(test)
+    test.globs["getRootFolder"] = davlayer.getRootFolder
+
+def tearDown(test):
+    z3c.dav.testing.functionalTearDown(test)
+    del test.globs["getRootFolder"]
 
 
 def test_suite():
-    properties = zope.file.testing.FunctionalBlobDocFileSuite(
+    properties = doctest.DocFileSuite(
         "properties.txt",
-        setUp = z3c.dav.testing.functionalSetUp,
-        tearDown = z3c.dav.testing.functionalTearDown,
+        setUp = setUp,
+        tearDown = tearDown,
         checker = z3c.etree.testing.xmlOutputChecker,
         optionflags = doctest.REPORT_NDIFF | doctest.NORMALIZE_WHITESPACE)
-    properties.layer = ZopeFileDAVLayer
+    properties.layer = davlayer
 
-    defaultview = zope.file.testing.FunctionalBlobDocFileSuite(
+    defaultview = doctest.DocFileSuite(
         "defaultview.txt",
-        setUp = z3c.dav.testing.functionalSetUp,
-        tearDown = z3c.dav.testing.functionalTearDown,
+        setUp = setUp,
+        tearDown = tearDown,
         checker = z3c.etree.testing.xmlOutputChecker,
         optionflags = doctest.REPORT_NDIFF | doctest.NORMALIZE_WHITESPACE)
-    defaultview.layer = ZopeFileDAVLayer
+    defaultview.layer = davlayer
                                       
     return unittest.TestSuite((
         doctest.DocTestSuite("z3c.davapp.zopefile"),
